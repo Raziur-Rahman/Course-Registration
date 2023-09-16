59d94201abc1af052c3d48ca3a62488a3ed55d5f
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-09-16 15:04:41 +0600

    first commit components are created and tailwindcss and daisyUI are installed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './Components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-6xl text-center text-[#1C1B1B] font-bold m-5 p-5'>Course Registration</h1>
+      </div>
+      <div className='mx-5 xl:mx-28'>
+        <Home></Home>
+      </div>
+
+    </>
+  )
+}
+
+export default App
