4073978abcbc6d0f51d6abbf7c051fcbf12f2197
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-09-16 16:12:11 +0600

    Cart Section Created and ready for use dynamic data

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,6 +4,27 @@ function Cart() {
 
     return (
         <>
+            <div className="p-5 bg-white rounded-lg">
+                <div>
+                    <p className="text-blue-600 text-xl font-semibold">Credit Hour Remaining  hr</p>
+                    <hr class="mt-2  h-[2px] bg-gray-400 opacity-40 dark:opacity-80" />
+                </div>
+                <div>
+                    <h1 className="text-3xl text-[#1C1B1B] font-semibold my-3">Course Name</h1>
+                    <ol className="list-decimal  list-inside text-base font-normal">
+                        
+                    </ol>
+                    <hr class="mt-2  h-[2px] bg-gray-400 opacity-40 dark:opacity-80" />
+                </div>
+                <div>
+                    <p className="mt-2">Total Credit Hour : </p>
+                    <hr class="mt-2  h-[2px] bg-gray-400 opacity-40 dark:opacity-80" />
+                </div>
+                <div>
+                    <p className="mt-2">Total Price :  USD</p>
+                    <hr class="mt-2  h-[2px] bg-gray-400 opacity-40 dark:opacity-80" />
+                </div>
+            </div>
 
         </>
     )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,19 @@
+import Cart from "../Cart/Cart"
 
 
 function Home() {
 
     return (
         <>
+            <div className="flex flex-col-reverse lg:flex-row">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:w-3/4">
+
+                </div>
+                <div className=" lg:w-1/4 my-5 md:w-1/2 md:mx-auto lg:ml-5 lg:my-0  ">
+                    <Cart></Cart>
+
+                </div>
+            </div>
 
         </>
     )
